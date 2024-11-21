49dd8e3fdfe8b38e9ae70a8fe677fde1465e3471
Mark Baggett <markpbaggett@gmail.com>
2024-11-20 20:29:36 -0600

    Add thumbnails.

--- src/manifest/manifest.py
+++ src/manifest/manifest.py
@@ -1,5 +1,6 @@
 from iiif_prezi3 import Manifest, config, KeyValueString
 import json
+import httpx
 
 
 class ManifestCreator:
@@ -25,14 +26,36 @@ class ManifestCreator:
                 )
         return metadata
 
+    @staticmethod
+    def _get_thumbnail(url):
+        image_response = httpx.get(f"{url}/info.json", timeout=60).json()
+        size = image_response['sizes'][2]
+        return {
+            "id": f"{url}/full/{size['width']},/0/default.jpg",
+            "width": size['width'],
+            "height": size['height'],
+            "type": "Image",
+            "format": "image/jpeg",
+            "service": [
+                {
+                    "id": url,
+                    "type": "ImageService3",
+                    "profile": "level2"
+                }
+            ]
+        }
+
     def _build(self):
+        thumbnail = self._get_thumbnail(self.data['iiif_url'])
         manifest = Manifest(
             id=f"https://markpbaggett.github.io/corpra-service-maps/manifests/{self.data['id']}.json",
             label=self.label,
             metadata=self.metadata,
+            thumbnail=thumbnail,
         )
         canvas = manifest.make_canvas_from_iiif(
             url=self.data['iiif_url'],
+            thumbnail=thumbnail,
             label=f"Canvas for {self.label}",
             id=f"https://markpbaggett.github.io/corpra-service-maps/{self.data['id']}/canvas/1",
             anno_id=f"https://markpbaggett.github.io/corpra-service-maps/{self.data['id']}/canvas/1/annotation/1",
